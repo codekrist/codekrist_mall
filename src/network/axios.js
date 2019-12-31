7aeb27de73af9d76f6cf24b8e0f8aba29ba78fca
codekrist <15510842745@163.com>
2019-12-31 17:56:24 +0800

    修改ip地址

--- src/network/axios.js
+++ src/network/axios.js
@@ -5,7 +5,7 @@ export default function axios(option) {
   return new Promise((resolve, reject) => {
     // 1.创建axios的实例
     const instance = originAxios.create({
-		baseURL: 'http://106.54.54.237:8000/api/v1',
+		baseURL: 'http://106.54.54.237:8000/api/w1',
       timeout: 5000
     });
 
